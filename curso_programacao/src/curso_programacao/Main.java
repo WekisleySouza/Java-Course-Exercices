36f5cd8d29efe42e2ebef76018f86b0f7a4edb18
Wekisley Souza <wekisleysouza.a@gmail.com>
2022-04-09 06:38:05 -0300

    Iniciando curso.

---
+++ curso_programacao/src/curso_programacao/Main.java
@@ -0,0 +1,12 @@
+package curso_programacao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int y = 32;
+		double x = 10.32123;
+		System.out.printf("%.2f", x);
+		System.out.println("Bom dia!");
+	}
+	
+}
